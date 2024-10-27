2c8eecbff34d2697e695bc9999d4604be158a334
Muhammad Husnain <muhammadhusnain472633@gmail.com>
2024-10-28 03:51:55 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:devfest/Screens/Home_Screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Myapp());
+}
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: HomeScreen()
+    ),
+    );
+  }
+}
